45b72d79b4963da3d2f2f23ca7652f1c5db81715
Sarang Arora <sarang.kaku11@gmail.com>
2022-01-12 17:13:27 +0530

    completed

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -10,7 +10,7 @@ const NewsItem =(props)=>{
                     <span className={`badge rounded-pill bg-${color}`}> {source}
                     </span>
                     </div>
-                    <img src={imageUrl ? imageUrl : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIvAyt56wZnto4FKG5B_HnP_jsqjZeWgPBWA&usqp=CAU'} className="card-img-top" alt="..." />
+                    <img src={(imageUrl===null || imageUrl==='') ?'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIvAyt56wZnto4FKG5B_HnP_jsqjZeWgPBWA&usqp=CAU'  : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
